c01782d6d1a93b81ae73612cfd53bffc402bfa58
felikshetalia <mccorsica@yahoo.com>
2026-07-03 08:47:44 +0200

    initial commit

---
+++ felikshetalia.MathGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type this...");
+            Console.ReadLine();
+        }
+    }
+}
